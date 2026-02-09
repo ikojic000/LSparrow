10b55a44a8ec70b233de53d1fe9f6e694dd6d94e
Ivan Kojić <ivank@teched.hr>
2026-02-09 11:55:32 +0100

    minor

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 @echo off
-echo Starting CSV Statistics Calculator...
+echo Starting LSparrow...
 echo.
 
-cd /d "C:\Users\IvanK\Python Projects\DiplomskiCSVStatistika"
-call "C:\Users\IvanK\Python Projects\Virtual ENVs\Diplomski_CSV_Statistics\Scripts\activate.bat"
+cd /d "C:\Users\IvanK\Python Projects\LSparrow"
+call "C:\Users\IvanK\Python Projects\Virtual ENVs\LSparrow\Scripts\activate.bat"
 
 echo Virtual environment activated.
 echo Starting Flask server at http://127.0.0.1:5000
